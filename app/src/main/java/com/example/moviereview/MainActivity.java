c194ce5c914bdbe8839ea69c4a184974cd78020a
kkw564 <kkw564@naver.com>
2019-07-08 23:18:45 +0900

    Hide an action bar

--- app/src/main/java/com/example/moviereview/MainActivity.java
+++ app/src/main/java/com/example/moviereview/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         upCount = (TextView)findViewById(R.id.up_count);
         downCount = (TextView)findViewById(R.id.down_count);
