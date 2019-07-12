0575419515ea998c6f0d78f1499f6223b9cf8888
kkw564 <kkw564@naver.com>
2019-07-12 18:36:06 +0900

    Make a Movie Review layout
    1. drawble에서 다양한 도형 생성
    2. Ratingbar 및 다양한 레이아웃 구성
    3. 추가적으로 필요한 구성요소 작성

--- app/src/main/java/com/example/moviereview/MainActivity.java
+++ app/src/main/java/com/example/moviereview/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.moviereview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView upCount;
     TextView downCount;
+
     ImageButton thumbUp;
     ImageButton thumbDown;
 
+    RatingBar ratingBar;
+    TextView ratingScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 downCount.setText(Integer.parseInt(downCount.getText().toString()) + 1 + "");
+            }
+        });
+
+        ratingBar = (RatingBar)findViewById(R.id.rating_bar);
+        ratingScore = (TextView)findViewById(R.id.rating_score_text_view);
+        /**
+         * Make a float value for rating default value in dimen.xml
+         */
+        TypedValue typedValue = new TypedValue();
+        getResources().getValue(R.dimen.rating_default_value, typedValue, true);
+        float ratingDefaultValue = typedValue.getFloat();
+        ratingBar.setRating(ratingDefaultValue);
+        ratingScore.setText(String.format("%.1f", ratingDefaultValue * 2));
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float floatRating, boolean fromUser) {
+                String strRating = String.format("%.1f", floatRating * 2);
+                ratingScore.setText(strRating);
             }
         });
     }
