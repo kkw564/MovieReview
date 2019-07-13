05126dc488437d8dfe93f45befcb01bd11c9a89f
kkw564 <kkw564@naver.com>
2019-07-13 18:14:17 +0900

    Make a comment list view
    1. 한줄평 아이콘 둥글게 표시
    2. 어댑터 이용하여 리스트뷰 생성
    3. 레이아웃 수정

--- app/src/main/java/com/example/moviereview/MainActivity.java
+++ app/src/main/java/com/example/moviereview/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.moviereview;
 
+import android.content.DialogInterface;
+import android.graphics.Point;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TextView upCount;
@@ -18,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     RatingBar ratingBar;
     TextView ratingScore;
+
+    ListView commentListView;
+
+    Button commentWrite;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +47,11 @@ public class MainActivity extends AppCompatActivity {
         thumbUp = (ImageButton)findViewById(R.id.btn_thumb_up);
         thumbDown = (ImageButton)findViewById(R.id.btn_thumb_down);
 
+        commentListView = (ListView) findViewById(R.id.comment_list_view);
+
+        CommentAdapter adapter = new CommentAdapter();
+        commentListView.setAdapter(adapter);
+        //adapter.addItem(new Sing);
         thumbState = 0;
 
         thumbUp.setOnClickListener(new View.OnClickListener(){
@@ -69,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
         /*
         thumbUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,5 +126,82 @@ public class MainActivity extends AppCompatActivity {
                 ratingScore.setText(strRating);
             }
         });
+
+
+        commentWrite = (Button)findViewById(R.id.comment_write_button);
+
+        commentWrite.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                showMessage();
+            }
+        });
+    }
+    public void showMessage(){
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+
+        builder.setTitle("안내");
+        builder.setMessage("종료하시겠습니까?");
+
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String message = "확인 감사합니다";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String message = "취소 감사합니다.";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+        Display display = getWindowManager(). getDefaultDisplay();
+        Point size = new Point();
+        display. getSize(size);
+        int width = size.x - 50;
+        int height = size.y - 50;
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getWindow().setLayout(width, height);
+    }
+
+    class CommentAdapter extends BaseAdapter{
+        ArrayList<CommentItem> items = new ArrayList<>();
+
+        public void addItem(CommentItem item){
+            items.add(item);
+        }
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            CommentItemView view = new CommentItemView(getApplicationContext());
+
+            CommentItem item = items.get(position);
+            view.setUserId(item.getId());
+            view.setComment(item.getComment());
+            view.setProfileImage(item.getProfileImage());
+            view.setRatingBar(item.getRatingBar());
+            view.setRecommendationCount(item.getRecommendationCount());
+            view.setTime(item.getTime());
+            return view;
+        }
     }
 }
