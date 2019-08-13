9c8b7f2c652bc3352fcdc0b27005efa38117b048
kkw564 <kkw564@naver.com>
2019-08-14 00:18:34 +0900

    Continue creating viewpager

--- app/src/main/java/com/example/moviereview/MainActivity.java
+++ app/src/main/java/com/example/moviereview/MainActivity.java
@@ -3,17 +3,39 @@ package com.example.moviereview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.vp_movie_slider);
+        // Cashing view pagers(fragments)
+        pager.setOffscreenPageLimit(3);
+
+        MoviePagerAdapter adapter = new MoviePagerAdapter(getSupportFragmentManager());
+        FragmentMovie1 fragmentMovie1 = new FragmentMovie1();
+        FragmentMovie2 fragmentMovie2 = new FragmentMovie2();
+        FragmentMovie3 fragmentMovie3 = new FragmentMovie3();
+
+        adapter.addItem(fragmentMovie1);
+        adapter.addItem(fragmentMovie2);
+        adapter.addItem(fragmentMovie3);
+
+        pager.setAdapter(adapter);
+
         initToolbar();
 
         findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
@@ -27,6 +49,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    class MoviePagerAdapter extends FragmentStatePagerAdapter{
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+        public MoviePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+    }
     private void initToolbar(){
         Toolbar toolbar = findViewById(R.id.tb_custom_toolbar);
         setSupportActionBar(toolbar);
